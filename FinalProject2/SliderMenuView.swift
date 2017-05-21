6bd860d9c2f48d9c1d37934ea1d8db996d5b88c9
Obiet Panggrahito <obiet.musicscape@gmail.com>
2017-05-21 15:18:26 +0800

    no message

---
+++ FinalProject2/SliderMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  SliderMenuView.swift
+//  FinalProject2
+//
+//  Created by Obiet Panggrahito on 21/05/2017.
+//  Copyright © 2017 Burst. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class SliderMenuView: UIView {
+
+    override func draw(_ rect: CGRect) {
+         UserInterfaceDesign.drawSliderMenu(frame: self.bounds, resizing: .aspectFill)
+    }
+
+}
